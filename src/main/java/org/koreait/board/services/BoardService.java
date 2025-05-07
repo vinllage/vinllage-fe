0f864c214abc15c6b7c35d09b47fdb74ec963fe0
chltkdwns <chtkdldjs43@naver.com>
2025-05-07 15:09:14 +0900

    강사님 내용

---
+++ src/main/java/org/koreait/board/services/BoardService.java
@@ -0,0 +1,26 @@
+package org.koreait.board.services;
+
+
+import org.koreait.board.mappers.BoardMapper;
+import org.koreait.board.validators.BoardsaveValidator;
+import org.koreait.global.configs.DBConn;
+import org.koreait.global.services.Bean;
+import org.koreait.global.services.Configuration;
+
+@Configuration
+public class BoardService {
+    @Bean
+    public BoardMapper boardMapper(){
+        return DBConn.getInstance().getSession().getMapper(BoardMapper.class);
+    }
+
+    @Bean
+    public BoardsaveValidator boardsaveValidator(){
+        return new BoardsaveValidator();
+    }
+
+    @Bean
+    public BoardSaveService saveService() {
+        return new BoardSaveService(boardMapper(), boardsaveValidator());
+    }
+}
---
+++ src/main/java/org/koreait/board/validators/BoardsaveValidator.java
@@ -0,0 +1,11 @@
+package org.koreait.board.validators;
+
+import org.koreait.board.controllers.RequestBoard;
+import org.koreait.global.validators.Validator;
+
+public class BoardsaveValidator implements Validator<RequestBoard> { //수정+
+    @Override
+    public void check(RequestBoard form){
+
+    }
+}
