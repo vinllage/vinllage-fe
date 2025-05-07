436c3aa57d581833ab6ea8e2d1dc9a74b6c62daf
chltkdwns <chtkdldjs43@naver.com>
2025-05-07 12:15:36 +0900

    VO 완성

---
+++ src/main/java/org/koreait/board/entities/Board.java
@@ -0,0 +1,11 @@
+package org.koreait.board.entities;
+
+import java.time.LocalDateTime;
+
+public class Board {
+    private int seq;
+    private String name;
+    private String title;
+    private String contents;
+    private LocalDateTime regDt;
+}
