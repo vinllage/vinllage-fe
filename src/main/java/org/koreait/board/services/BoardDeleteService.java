d0794281a5fc027166e3a98f8b1ba86984b51de4
KimMarinet <kiana507680@gmail.com>
2025-05-08 03:28:36 +0900

    Delete Complete

--- src/main/java/org/koreait/board/services/BoardDeleteService.java
+++ src/main/java/org/koreait/board/services/BoardDeleteService.java
@@ -1,7 +1,17 @@
 package org.koreait.board.services;
 
+import org.koreait.board.mappers.BoardMapper;
 import org.koreait.global.validators.RequiredFieldValidator;
 
 public class BoardDeleteService implements RequiredFieldValidator {
+    private BoardMapper mapper;
 
+    public BoardDeleteService(BoardMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    public void process(long seq) {
+        checkTrue(mapper.exists(seq) == 1, "게시글을 찾을 수 없습니다.");
+        mapper.delete(seq);
+    }
 }
