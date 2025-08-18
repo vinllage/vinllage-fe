8bd233b06687aa09a95d99dc2a2e4ec990a1372c
leesomin1204 <sm1157@naver.com>
2025-08-18 13:01:10 +0900

    기본작업

---
+++ src/app/_global/outlines/Footer.tsx
@@ -0,0 +1,8 @@
+'use client'
+import React from 'react'
+
+const Footer = () => {
+    return <h1>푸터영역</h1>
+}
+
+export default React.memo(Footer)
---
+++ src/app/_global/outlines/Header.tsx
@@ -0,0 +1,8 @@
+'use client'
+import React from 'react'
+
+const Header = () => {
+    return <h1>헤더영역</h1>
+}
+
+export default React.memo(Header)
