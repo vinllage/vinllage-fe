85f3aa8456a16cfb4a9038736b1d74050e7ad670
songmikim <ksm0200@gmail.com>
2025-08-20 18:21:02 +0900

    환경행사 타이틀 추가

--- src/app/event/[hash]/page.tsx
+++ src/app/event/[hash]/page.tsx
@@ -1,4 +1,5 @@
 import EventDetailContainer from '../_containers/EventDetailContainer'
+import { MainTitle } from '../../_global/components/TitleBox'
 import { getEvent } from '../_services/actions'
 import type { EventType } from '../_types'
 
@@ -11,5 +12,10 @@ export default async function EventDetailPage({
   if (!event) {
     return null
   }
-  return <EventDetailContainer event={event} />
+  return (
+    <div className="layout-width">
+      <MainTitle border="true">환경 행사</MainTitle>
+      <EventDetailContainer event={event} />
+    </div>
+  )
 }
