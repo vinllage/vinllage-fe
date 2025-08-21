7a288a2f1b8c5b992516365c1ffedfd4933b4722
leesomin1204 <sm1157@naver.com>
2025-08-21 21:57:32 +0900

    Update server.config.js
    vinnlage -> vinllage 오타 수정

--- src/app/server.config.js
+++ src/app/server.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'vlnnlage-fe',
+      name: 'vlnllage-fe',
       script: 'node_modules/next/dist/bin/next',
       args: 'start -p 3100',
       instances: 1,
