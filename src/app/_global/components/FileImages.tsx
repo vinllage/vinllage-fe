4357a29b9648f0081b58d2fae1ac6ec4159f3a9d
leesomin1204 <sm1157@naver.com>
2025-08-21 18:24:06 +0900

    profile

---
+++ src/app/_global/components/FileImages.tsx
@@ -0,0 +1,111 @@
+'use client'
+import React, { useState, useCallback } from 'react'
+import styled from 'styled-components'
+import Image from 'next/image'
+import { FaRegWindowClose } from 'react-icons/fa'
+import LayerPopup from './LayerPopup'
+import useFetchCSR from '../hooks/useFetchCSR'
+import color from '../styles/color'
+const { dark, white } = color
+
+const ImageItems = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  li {
+    border: 3px solid ${dark};
+    position: relative;
+    margin: 3px 0;
+    border-radius: 3px;
+
+    .remove {
+      position: absolute;
+      top: 5px;
+      right: 5px;
+      cursor: pointer;
+      font-size: 1.5rem;
+      color: ${white};
+    }
+
+    img {
+      cursor: pointer;
+      display: block;
+    }
+  }
+
+  li + li {
+    margin-left: 5px;
+  }
+`
+
+type FileType = {
+  items: any
+  width?: number
+  height?: number
+  callback?: (item: any) => void
+}
+
+const ImageItem = ({ item, width, height, callback }) => {
+  const { seq, fileUrl, thumbBaseUrl, fileName, image } = item
+  const [open, setOpen] = useState<boolean>(false)
+  const fetchCSR = useFetchCSR()
+
+  const onClose = useCallback(() => setOpen(false), [])
+  const onShow = useCallback(() => setOpen(true), [])
+
+  const onRemove = useCallback((seq) => {
+    fetchCSR(`/file/delete/${seq}`, {method: 'DELETE'})
+    .then(res => res.json())
+    .then(item => {
+        // 삭제 후 후속처리
+        if (typeof callback == 'function') {
+            callback(item)
+        }
+    })
+  }, [fetchCSR]);
+
+  return (
+    image && (
+      <li>
+        <FaRegWindowClose className="remove" onClick={() => onRemove(seq)} />
+        <Image
+          src={`${thumbBaseUrl}&width=${width}&height=${height}&crop=true`}
+          alt={fileName}
+          width={width}
+          height={height}
+          onClick={onShow}
+        />
+        <LayerPopup width={500} isOpen={open} onClose={onClose}>
+          <Image
+            className="org-image"
+            width={500}
+            height={500}
+            src={fileUrl}
+            alt={fileName}
+          />
+        </LayerPopup>
+      </li>
+    )
+  )
+}
+
+const FileImages = ({ items, width, height, callback }: FileType) => {
+  items = Array.isArray(items) ? items : items ? [items] : []
+  if (items.length === 0) return <></>
+  width = width ?? 100
+  height = height ?? 100
+  return (
+    <ImageItems>
+      {items.map((item) => (
+        <ImageItem
+          key={'file-' + item.seq}
+          item={item}
+          width={width}
+          height={height}
+          callback={callback}
+        />
+      ))}
+    </ImageItems>
+  )
+}
+
+export default React.memo(FileImages)
--- src/app/member/_components/JoinForm.tsx
+++ src/app/member/_components/JoinForm.tsx
@@ -4,7 +4,9 @@ import { MdCheckBox, MdCheckBoxOutlineBlank } from 'react-icons/md'
 import { Input } from '@/app/_global/components/Forms'
 import { SubmitButton } from '@/app/_global/components/Buttons'
 import MessageBox from '@/app/_global/components/MessageBox'
-import FileBox from '@/app/_global/components/FileBox'
+import FileUpload from '@/app/_global/components/FileUpload'
+import FileImages from '@/app/_global/components/FileImages'
+import FileItems from '@/app/_global/components/FileItems'
 
 const StyledForm = styled.form`
   .message {
@@ -12,9 +14,10 @@ const StyledForm = styled.form`
   }
 `
 
-const JoinForm = ({ errors, action, pending, onChange, onToggle, form }) => {
+const JoinForm = ({ errors, action, pending, onChange, onToggle, form, fileUploadCallback, fileDeleteCallback }) => {
   return (
     <StyledForm action={action} autoComplete="off">
+      <input type="hidden" name="gid" value={form.gid} />
       <input type="hidden" name="termsAgree" value={form.termsAgree} />
       <Input
         type="text"
@@ -62,7 +65,15 @@ const JoinForm = ({ errors, action, pending, onChange, onToggle, form }) => {
       <MessageBox color="danger">{errors?.mobile}</MessageBox>
 
       <h3>프로필 이미지</h3>
-      <FileBox />
+
+      <FileImages items={form.profileImage} callback={fileDeleteCallback}/>
+      <FileItems items={form.profileImage} />
+      <FileUpload
+        gid={form.gid}
+        imageOnly={true}
+        single={true} // 여러개 업로드 필요시 false
+        callback={fileUploadCallback}
+      />
 
       <h3>약관동의</h3>
       <div>약관 동의 작성...</div>
