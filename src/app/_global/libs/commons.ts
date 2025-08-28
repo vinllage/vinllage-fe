52cb8be10d8a9a37b27ff72bb7aace72b6439d46
dkstmfrl020527 <slgian527@gmail.com>
2025-08-28 13:04:40 +0900

    게시글 수정 기능

--- src/app/_global/libs/commons.ts
+++ src/app/_global/libs/commons.ts
@@ -30,3 +30,24 @@ export function passwordStrenthLevel(password?: string | number) {
 
   return strenth
 }
+
+/**
+ * FormData 형식의 데이터를 일반 자바스크립트 객체로 변환
+ * @param formData
+ */
+export function toPlainObj(formData: FormData) {
+  const params: any = {}
+
+  // 필요한 필드와 값만 추출
+  for (const [key, value] of formData.entries()) {
+    if (key.startsWith('$ACTION_')) continue
+    let _value: string | boolean = value.toString()
+    if (['true', 'false'].includes(_value)) {
+      _value = _value === 'true'
+    }
+
+    params[key] = _value
+  }
+
+  return params
+}
